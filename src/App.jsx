7dade8c95a3c2aabc97e0ecc33a34600fb89aa23
gabriel <gabriel.m.ledesma96@gmail.com>
2023-08-10 20:24:23 -0300

    hash router

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import  'bootstrap/dist/css/bootstrap.min.css' ;
 import NavBar from './components/NavBar';
 import Footer from "./components/Footer";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {HashRouter as Router, Route, Routes} from 'react-router-dom'
 import Home from './components/Home';
 import Contact from './components/Contact';
 import AboutUs from './components/AboutUs';
@@ -18,7 +18,7 @@ ReactPixel.init('824213002605364', null, options);
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <NavBar/>
       <Routes>
         <Route exact path='/' element={<Home/>}/>
@@ -26,7 +26,7 @@ function App() {
         <Route exact path='/aboutus' element={<AboutUs/>}/>
       </Routes>
       <Footer/>
-    </BrowserRouter>
+    </Router>
   )
 }
 
--- src/components/Whatsapp.jsx
+++ src/components/Whatsapp.jsx
@@ -5,7 +5,7 @@ const Whatsapp = () => {
   }
   return (
     <div className="botton">
-        <a onClick={handleClickWhatsapp} href={`https://api.whatsapp.com/send?phone=5491138540082&text=Hola%20Flor!%20Me%20gustaría%20arrancar%20con%20el%20Italiano...`} target="blank">
+        <a onClick={handleClickWhatsapp && fbq('track', 'Contact')} href={`https://api.whatsapp.com/send?phone=5491138540082&text=Hola%20Flor!%20Me%20gustaría%20arrancar%20con%20el%20Italiano...`} target="blank">
             <img src="/whatsapp_logo_icon_229310.png" alt="icono de WhastsApp" />
         </a>
     </div>
